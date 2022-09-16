b519bbc1086c3b20adf390f34ef0e12bca6d4d95
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-17 00:06:41 +0600

    Add files via upload

---
+++ Array concat method/playGround.js
@@ -0,0 +1,5 @@
+var number=[1,2,3,4,5,6,7]
+var number2=[8,9,10,11,12,13]
+var result= number.concat(number2);
+
+console.log(result)//num1+num2
---
+++ Array findIndex method/playGround.js
@@ -0,0 +1,5 @@
+var number=[1,2,3,4,5,6,7]
+var result= number.find((currentValue)=>{
+    return currentValue>4;
+})
+console.log(result)
