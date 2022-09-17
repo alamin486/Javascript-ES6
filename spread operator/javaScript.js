682be975219c6b16d72c81a5fff168923d04affe
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-18 00:06:05 +0600

    Add files via upload

---
+++ spread operator/javaScript.js
@@ -0,0 +1,4 @@
+var numbers=[1,2,3]
+//var newNumbers=[numbers[0],numbers[1],numbers[2],4,5,6]
+var newNumbers=[...numbers, 4, 5, 6]
+console.log(newNumbers)
---
+++ spread operator/javaScript2.js
@@ -0,0 +1,5 @@
+var numbers=[1,2,3]
+var numbers2=[6,7,8]
+//var newNumbers=[numbers[0],numbers[1],numbers[2],4,5,6]
+var newNumbers=[...numbers,...numbers2]
+console.log(newNumbers)
