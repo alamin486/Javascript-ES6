05e2fc9bd1cd81bd803fc685d90263f2af10730b
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-18 00:05:14 +0600

    Add files via upload

---
+++ Important object tricks/javaScript.js
@@ -0,0 +1,13 @@
+var x=20;
+var y=10;
+var z=x*y;
+var myObj1 = {
+    name:"Js",
+    estd:"1995",
+    founder:"Berden Eich",
+    x,
+    y,
+    z
+};
+
+console.log(myObj1)
---
+++ Important object tricks/javaScript1.js
@@ -0,0 +1,11 @@
+var myObj1 = {
+    name:"Js",
+    estd:"1995",
+    founder:"Berden Eich"
+};
+var keys=object.keys(myObj1);
+var values=object.values(myObj1);
+var entries=object.entries(myObj1);
+console.log(myObj1)
+console.log(values)
+console.log(entries)
