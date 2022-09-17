cca80c9eff1d19ad42ad4debd69d5d5491a86677
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-18 00:05:49 +0600

    Add files via upload

---
+++ Rest operator/javaScript.js
@@ -0,0 +1,4 @@
+function myFunc(...params){
+    console.log(params)
+}
+myFunc(5,6,7,8,9,10)
---
+++ Rest operator/javaScript1.js
@@ -0,0 +1,5 @@
+function myFunc(a,...params){
+    console.log(a)
+    console.log(params)
+}
+myFunc(5,6,7,8,9,10)
